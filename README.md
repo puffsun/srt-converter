c3546e592ddac0cf68ac1997f2c09667457cda71
George Sun <sunwinner3@gmail.com>
2017-05-17 16:09:10 +0800

    with glob.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 #  convert srt subtitles to webvtt
 
+转换 srt 格式的字母为 webvtt 格式。默认递归读取当前目录下所有的 srt 文件并转换，可以用 `-s` 或者 `--source-folder` 指定读取的字母文件目录。
+
+默认情况下将文件保存到源文件同一目录下，可以使用 `-t` 或者 `--target-folder` 指定目标目录。
+
 ## Usage
+
+```bash
+node index.js -s /path/to/folder
+```
+or
+```bash
+node index.js --source-folder /path/to/folder
+```
--- index.js
+++ index.js
@@ -2,33 +2,29 @@ var fs = require('fs');
 var path = require('path');
 var srt2vtt = require('srt-to-vtt');
 var parseArgs = require('minimist');
+var glob = require('glob');
 var _ = require('lodash');
 
 var argv = parseArgs(process.argv.slice(2));
 var sourceFolder = argv['s'] || argv['source-folder'] || '.';
-var targetFolder = argv['t'] || argv['target-folder'] || '.';
+var targetFolder = argv['t'] || argv['target-folder'];
 var files = argv['_'];
 
 function convertSrtFile(src) {
-    var target = path.basename('src', path.extname(src)) + '.vtt';
+    var filename = path.basename(src, path.extname(src)) + '.vtt';
+    targetFolder = targetFolder || path.dirname(src);
+    var target = path.join(targetFolder, filename);
     fs.createReadStream(src)
         .pipe(srt2vtt())
         .pipe(fs.createWriteStream(target))
     console.info('convert file %s to %s succeed.', src, target);
 }
 
-fs.readdir(sourceFolder, function(err, files) {
-    if (err) {
-        console.error('read folder %s with error.', sourceFolder);
-        return;
-    }
-    files.map(function(file) {
-        return path.join(sourceFolder, file);
-    }).filter(function(file) {
-        var stat = fs.statSync(file);
-        var extname = path.extname(file);
-        return stat.isFile() && extname === '.srt';
-    }).forEach(function(file) {
+glob('**/*.srt', {
+    cwd: sourceFolder,
+    absolute: true
+}, function(err, files) {
+    files.forEach(function(file) {
         convertSrtFile(file);
     });
 });
